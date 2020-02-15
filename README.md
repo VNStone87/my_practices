7d0d14dfb5c29cbaacb9fd7763b5af2069cca3a6
VNStone87 <leandtvt@gmail.com>
2020-02-15 21:02:26 +0700

    let's begin!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # This is my practices for sysadmin.
+** This is my readme
---
+++ openvpn/managing_ouser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUTDIR=/etc/openvpn/client-config
+KEYDIR=/etc/openvpn/client
+BASEFILE=/etc/openvpn/client-config/client
+
+function adduser() {
+ouser=$1
+echo "Adding..."
+}
+

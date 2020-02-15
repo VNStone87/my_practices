c278fad052120a442cdb52b07dc5b1a6627e938c
VNStone87 <leandtvt@gmail.com>
2020-02-15 21:26:07 +0700

    doing

--- openvpn/managing_ouser.sh
+++ openvpn/managing_ouser.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 OUTDIR=/etc/openvpn/client-config
 KEYDIR=/etc/openvpn/client
-BASEFILE=/etc/openvpn/client-config/client
-
+TEMPFILE=/etc/openvpn/client-config/client
+RUNDIR=/etc/openvpn/easyrsa/3
+RUNFILE=/etc/openvpn/easyrsa/3/easyrsa
 function adduser() {
-ouser=$1
+arg1=$1
+#arg2=$2
+if [$arg1 == ""]; {
+echo "Nothing is enter for creating. Try again!"
+exit 1
+}
 echo "Adding..."
+$RUNFILE gen-req $arg1 nopass
+$RUNFILE sign-req client $arg1
+cp ${RUNDIR}/pki/issued/${arg1}.crt $KEYDIR
+cp ${RUNDIR}/pki/private/${arg1}.key $KEYDIR
+echo "Done."
 }
 

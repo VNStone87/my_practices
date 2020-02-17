293748f152b38ef8b22ff88648270c7ab8f19d92
VNStone87 <44486536+VNStone87@users.noreply.github.com>
2020-02-17 11:09:25 +0700

    test done

--- openvpn/managing_ouser.sh
+++ openvpn/managing_ouser.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 OUTDIR=/etc/openvpn/client-config
-KEYDIR=/etc/openvpn/client
-TEMPFILE=/etc/openvpn/client-config/client
-RUNDIR=/etc/openvpn/easyrsa/3
-RUNFILE=/etc/openvpn/easyrsa/3/easyrsa
-function adduser() {
-arg1=$1
-#arg2=$2
-if [$arg1 == ""]; {
-echo "Nothing is enter for creating. Try again!"
-exit 1
+TEMFILE=/etc/openvpn/client-config/client.conf
+KEY_DIR=/etc/openvpn/client
+BASEDIR=/etc/openvpn/easy-rsa/3
+function addUser() {
+  ouser=$1
+  if [ "$ouser" == "" ]; then
+    echo "Not found username argument!"
+    exit 1
+  fi
+  OUTFILE=/etc/openvpn/client-config/$ouser.ovpn
+  echo "Adding User..."
+  cd $BASEDIR
+  ./easyrsa gen-req $ouser nopass
+  if [$? -ne 0]; then
+     echo "Running gen-req failed!"
+     exit 1
+  fi  
+  ./easyrsa sign-req client $ouser
+  if [$? -ne 0]; then
+     echo "Running sign-request failed!"
+     exit 1
+  fi
+  cp $BASEDIR/pki/issued/$ouser.crt $KEY_DIR
+  cp $BASEDIR/pki/private/$ouser.key $KEY_DIR
+  cat ${TEMFILE} \
+      <(echo -e '<cert>') \
+      ${KEY_DIR}/${ouser}.crt \
+      <(echo -e '</cert>\n<key>') \
+      ${KEY_DIR}/${ouser}.key \
+      <(echo -e '</key>') \
+      > ${OUTDIR}/${ouser}.ovpn
+  echo "Output file for OPENVPN client is created at $OUTFILE"
+  
 }
-echo "Adding..."
-$RUNFILE gen-req $arg1 nopass
-$RUNFILE sign-req client $arg1
-cp ${RUNDIR}/pki/issued/${arg1}.crt $KEYDIR
-cp ${RUNDIR}/pki/private/${arg1}.key $KEYDIR
+function main() {
+username=$1
+addUser $username
 echo "Done."
 }
-
+main $1

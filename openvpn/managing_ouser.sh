81d6d8a73e62703ee8480f0fda1b81361cff9033
VNStone87 <44486536+VNStone87@users.noreply.github.com>
2020-02-18 17:01:07 +0700

    fix error

--- openvpn/managing_ouser.sh
+++ openvpn/managing_ouser.sh
@@ -9,20 +9,12 @@ function addUser() {
   if [ -f ${KEY_DIR}/${ouser}.key]; then
      echo "User is existing!"
      exit 1
-  fi
+  else
   OUTFILE=/etc/openvpn/client-config/$ouser.ovpn
   echo "Adding User..."
   cd $BASEDIR
-  ./easyrsa gen-req $ouser nopass
-  if [$? -ne 0]; then
-     echo "Running gen-req failed!"
-     exit 1
-  fi  
+  ./easyrsa gen-req $ouser nopass 
   ./easyrsa sign-req client $ouser
-  if [$? -ne 0]; then
-     echo "Running sign-request failed!"
-     exit 1
-  fi
   cp $BASEDIR/pki/issued/$ouser.crt $KEY_DIR
   cp $BASEDIR/pki/private/$ouser.key $KEY_DIR
   cat ${TEMFILE} \
@@ -33,28 +25,29 @@ function addUser() {
       <(echo -e '</key>') \
       > ${OUTDIR}/${ouser}.ovpn
   echo "Output file for OpenVPN client is created at $OUTFILE"
-
+  fi
 function delUser() {
   duser=$1
   if [ ! -f ${KEY_DIR}/${ouser}.key ]; then
   echo "User not found!.Please check again"
-  fi
+  else
   cd $BASEDIR
   ./easyrsa revoke $duser
   ./easyrsa gen-crl
   cp -rf pki/crl.pem $CRLFILE
   /etc/init.d/openvpn restart
   echo "User is deleted!"
-   
+  fi 
 } 
  
 }
 function main() {
-if [! -f "$1"||"$2" ]; then
+if [[ -z $1 && -z $2 ]]; then
   echo "Please enter arguments again. "
   exit 1
-fi
+else
         username=$2
+fi
 case "$1" in
   add)
         addUser $username
